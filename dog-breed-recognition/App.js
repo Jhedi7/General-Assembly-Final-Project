f37926ec804f9dbb1b0c31f07eff23c0011f9700
Student_105 <student_105@ool-ad020213.dyn.optonline.net>
2018-05-24 16:21:01 -0400

    react navigator

--- dog-breed-recognition/App.js
+++ dog-breed-recognition/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Login from './src/components/Login'
+import Home from './src/components/Home'
 
 export default class App extends React.Component {
   render() {
@@ -8,6 +9,7 @@ export default class App extends React.Component {
       <View style={styles.container}>
 
         <Login />
+        <Home />
       </View>
     );
   }
