2875ccd275aa8ce1fe63973ceab15cbfb84e70fa
Student_105 <student_105@ool-ad020213.dyn.optonline.net>
2018-05-24 16:04:59 -0400

    App up and runnig

---
+++ dog-breed-recognition/src/components/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Login extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>AT THE LOGIN PAGE!!</Text>
+      </View>
+
+
+    );
+  }
+}
