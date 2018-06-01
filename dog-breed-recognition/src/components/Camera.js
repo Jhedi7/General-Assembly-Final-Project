838582bd57302557ec5a6e09ecd159aea34dceec
Student_105 <student_105@PSXGA105.home>
2018-06-01 19:57:10 -0400

    semi working clarifai

--- dog-breed-recognition/src/components/Camera.js
+++ dog-breed-recognition/src/components/Camera.js
@@ -15,15 +15,27 @@ import PropTypes from 'prop-types'
 import { Camera, Permissions } from 'expo';
 import Expo from 'expo';
 import { ImagePicker } from 'expo';
-import RNFetchBlob from "react-native-fetch-blob";
+// import RNFetchBlob from "react-native-fetch-blob";
 import Clarifai from "clarifai";
 
+//  this.app = new Clarifai.App({
+//   apiKey: "c362597d65354a998a07e5c6ba1da882"
+// });
+
 export default class CameraApp extends React.Component {
-  state = {
-    // imgUri: null,
-    topText: '',
-    bottomText: '',
+  constructor(props){
+    super(props);
+    this.state = {
+      imgUri: '',
+      data: {},
+      conceptsLoaded: false
   }
+  this.generateBreed = this.generateBreed.bind(this);
+  // this.imgUri = this.imgUri.bind(this)
+  // this.app = new Clarifai.App({
+  // apiKey: "c362597d65354a998a07e5c6ba1da882"
+
+}
 
 async  alertIfRemoteNotificationsDisabledAsync() {
   const { Permissions } = Expo;
@@ -34,45 +46,86 @@ async  alertIfRemoteNotificationsDisabledAsync() {
 }
 
  async componentWillMount() {
+
     const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
     this.setState({ permissionsGranted: status === 'granted' });
   }
 
+  componentWillUnmount (){
+    // this.generateBreed()
+  }
+
+choosePicture = async () => {
+    const {
+      cancelled,
+      uri,
+    } = await Expo.ImagePicker.launchImageLibraryAsync();
+    if (!cancelled) {
+      this.setState({ imgUri: uri });
+      console.log(uri) // this logs correctly
+
+    }
+  }
 
 
-  generateBreed = async function(){
+  takePicture = async () => {
+    const {
+      cancelled,
+      uri,
+    } = await Expo.ImagePicker.launchCameraAsync({});
+    if (!cancelled) {
+      this.setState({ imgUri: uri });
+    }
+  }
 
-    // save to CameraRoll
-    CameraRoll.saveToCameraRoll(this.state.path);
-    // convert the image to base64 data
-    RNFetchBlob.fs
-      .readFile(this.state.path, "base64")
-      .then(data => {
-        app.models
-          .predict(Clarifai.GENERAL_MODEL, { base64: data })
-          .then(response => {
+
+  savePicture = async () => {
+    const uri = await Expo.takeSnapshotAsync(this.imageView, {});
+    await CameraRoll.saveToCameraRoll(uri);
+
+  }
+
+
+
+  generateBreed =  () => {
+    const app = new Clarifai.App({
+  apiKey: "c362597d65354a998a07e5c6ba1da882"})
+
+    const data = this.state.imgUri;
+    // data.append('photo', {
+    //     uri: data,
+    //     type: 'image/jpeg',
+    //     name: 'testPhotoName'
+    //   });
+
+    process.nextTick = setImmediate
+
+   app.models.predict(Clarifai.GENERAL_MODEL, data )
+      .then(response => {
             // do something with response
             console.log("response from clarifai ", response);
             this.setState({
+
+              imgUri: response,
               data: response,
               conceptsLoaded: true
+
             });
-            this.createStringOfNames();
-          });
+            // console.log(imgUri)
 
-      })
-      .catch(error => {
-        // error on converting image to base64 data
-        console.log(error);
-      });
+          })
+      .catch(err => console.log(err))
+        }
 
-  }
 
 
 
 
 
   render() {
+    console.log(this.state.imgUri)
+    console.log(this.state.data)
+
     return (
       <View style={styles.container}>
 
@@ -113,7 +166,8 @@ async  alertIfRemoteNotificationsDisabledAsync() {
 
         <TouchableOpacity
             style={styles.button}
-            onPress={this.generateBreed}>
+            onPress={this.generateBreed}
+            >
             <Text style={styles.buttonText}>LOOK UP SOME DOGS!</Text>
           </TouchableOpacity>
 
@@ -123,35 +177,6 @@ async  alertIfRemoteNotificationsDisabledAsync() {
 
 
 
-  choosePicture = async () => {
-    const {
-      cancelled,
-      uri,
-    } = await Expo.ImagePicker.launchImageLibraryAsync();
-    if (!cancelled) {
-      this.setState({ imageUri: uri });
-      console.log(uri) // this logs correctly
-
-    }
-  }
-
-
-  takePicture = async () => {
-    const {
-      cancelled,
-      uri,
-    } = await Expo.ImagePicker.launchCameraAsync({});
-    if (!cancelled) {
-      this.setState({ imgUri: uri });
-    }
-  }
-
-
-  savePicture = async () => {
-    const uri = await Expo.takeSnapshotAsync(this.imageView, {});
-    await CameraRoll.saveToCameraRoll(uri);
-
-  }
 
 
 
@@ -182,123 +207,29 @@ const styles = StyleSheet.create({
 
 
 
+// // save to CameraRoll
+    // CameraRoll.saveToCameraRoll(this.state.path);
+    // // convert the image to base64 data
+    // RNFetchBlob.fs
+    //   .readFile(this.state.path, "base64")
+    //   .then(data => {
+    //     app.models
+    //       .predict(Clarifai.GENERAL_MODEL, { base64: data })
+    //       .then(response => {
+    //         // do something with response
+    //         console.log("response from clarifai ", response);
+    //         this.setState({
+    //           data: response,
+    //           conceptsLoaded: true
+    //         });
+    //         this.createStringOfNames();
+    //       });
+
+    //   })
+    //   .catch(error => {
+    //     // error on converting image to base64 data
+    //     console.log(error);
+    //   });
 
 
 
-// export default class Camera extends React.Component {
-//   constructor() {
-//   super()
-//   this.state = {
-
-//     loading: false // initializing loading state
-//   }
-//   this.imagePicker = this.imagePicker.bind(this)
-// }
-
-  // this.options = {
-  //     title: 'Sélectionner une image',
-  //     takePhotoButtonTitle: 'Prendre une photo',
-  //     chooseFromLibraryButtonTitle: 'Choisir depuis la galerie',
-  //     cancelButtonTitle: 'Annuler',
-  //     cameraType: 'back',
-  //     mediaType: 'photo',
-  //     storageOptions: {
-  //       skipBackup: true,
-  //       path: 'Seepizz'
-  //     }
-  //   }
-  // }
-
-
-
-
-  // onClick() {
-  //     this.setState({ loading: true })
-  //     ImagePicker.showImagePicker(this.options, response => {
-  //       if (response.didCancel) {
-  //         this.setState({ loading: false })
-  //       } else if (response.error) {
-  //         Alert.alert('Erreur', 'Vérifiez vos permissions aux albums photos et à la caméra.', { cancelable: false })
-  //         this.setState({ loading: false })
-  //       } else {
-  //         const { navigate } = this.props.navigation
-  //         navigate('Prediction', { image: response })
-  //         this.setState({ loading: false })
-  //       }
-  //     })
-  //   }
-
-
-
-
-
-
-//   imagePicker () {
-
-//     const options = {
-//       quality: 1.0,
-//       // maxWidth: 500,
-//       // maxHeight: 500,
-//       storageOptions: {
-//         skipBackup: true
-//       }
-//     }
-
-
-//     // Display the image picker menu, and log the response based on the user's actions
-//     ImagePickerManager.showImagePicker(options, response => {
-//       console.log('Image Picker Response:', response);
-//       if (response.didCancel) {
-//         console.log('User cancelled image picker');
-//       } else if (response.error) {
-//         console.log('ImagePicker Error: ', response.error);
-//       } else {
-//         // Update the state
-//         this.setState({
-//           photoInfo: response
-//         })
-//       }
-//     })
-//   };
-
-// componentDidMount () {
-//   this.imagePicker ()
-// }
-
-
-
-//   render() {
-//     return (
-//     <View style={styles.container}>
-//       <StatusBar hidden />
-
-//       {
-//         !this.state.loading ?
-//         <Button
-//          title='Analyse a picture!'
-//          onPress={this.imagePicker().bind(this)}
-//         /> :
-//         <ActivityIndicator size="large" color="#e74c3c" />
-//       }
-
-//     </View>
-//   )
-// }
-
-
-
-
-
-
-// }
-
-// // <BackgroundImage source={require('../../../wireframes/priority-matrix.JPG')}>
-
-// const styles = StyleSheet.create({
-//   container: {
-//     backgroundColor: 'white',
-//     width: '100%',
-//     flex: 1,
-//     justifyContent: 'center'
-//   }
-// })
